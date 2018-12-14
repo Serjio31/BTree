c6609aca35ea544d759e56de4088f164fef8e1ad
Serjio31 <s.kamnev@yandex.ru>
2018-12-15 00:02:53 +0300

    add search

--- src/main/kotlin/BTree.kt
+++ src/main/kotlin/BTree.kt
@@ -1,19 +1,26 @@
-class BTree(val t: Int) {
-
-    var keysCount: Int = 0
-        private set
-
-    var nodesCount: Int = 0
-        private set
-
-    var isLeaf: Boolean = true
-        private set
-
-    var keys: ArrayList<Int> = ArrayList()
-        private set
-
+class BTree(
+    val t: Int,
+    var keysCount: Int = 0,
+    var keys: ArrayList<Int> = ArrayList(),
+    var isLeaf: Boolean = true,
+    var nodesCount: Int = 0,
     var nodes: ArrayList<BTree> = ArrayList()
-        private set
+) {
+
+    fun search(key: Int): Pair<BTree, Int>? {
+        var i = 0
+        while (i <= keysCount && key > keys[i]) {
+            i++
+        }
+        if (i <= keysCount && key == keys[i]) {
+            return Pair(this, i)
+        }
+        if (isLeaf) {
+            return  null
+        } else {
+            return nodes[i].search(key)
+        }
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -23,6 +30,7 @@ class BTree(val t: Int) {
 
         if (t != other.t) return false
         if (keysCount != other.keysCount) return false
+        if (nodesCount != other.nodesCount) return false
         if (isLeaf != other.isLeaf) return false
         if (keys != other.keys) return false
         if (nodes != other.nodes) return false
@@ -33,6 +41,7 @@ class BTree(val t: Int) {
     override fun hashCode(): Int {
         var result = t
         result = 31 * result + keysCount
+        result = 31 * result + nodesCount
         result = 31 * result + isLeaf.hashCode()
         result = 31 * result + keys.hashCode()
         result = 31 * result + nodes.hashCode()
--- src/main/kotlin/BTreeTest.kt
+++ src/main/kotlin/BTreeTest.kt
@@ -8,4 +8,19 @@ class BTreeTest {
         val tree = BTree(2)
         assertTrue(tree == BTree(2))
     }
+
+    @Test
+    fun searchKey() {
+        val firstTree = BTree(2, 2, arrayListOf(1, 2))
+
+        val secondTree = BTree(2, 2, arrayListOf(4, 5))
+
+        val threeTree = BTree(2, 2, arrayListOf(7, 8))
+
+        val tree = BTree(
+            2, 2, arrayListOf(3, 6),
+            false, 3, arrayListOf(firstTree, secondTree, threeTree)
+        )
+        assertEquals(Pair(secondTree, 1), tree.search(5))
+    }
 }
