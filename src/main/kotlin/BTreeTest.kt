3cf0764c324f44de4991fec92093c1171e3dedce
Serjio31 <s.kamnev@yandex.ru>
2018-12-14 20:16:14 +0300

    create project

---
+++ src/main/kotlin/BTreeTest.kt
@@ -0,0 +1,7 @@
+import org.junit.jupiter.api.Assertions.assertAll
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.function.Executable
+
+class BTreeTest {
+}
