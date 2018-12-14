db47dd062950e3cd28ed366ea18c751650431919
Serjio31 <s.kamnev@yandex.ru>
2018-12-14 21:36:02 +0300

    add test for empty class

--- src/main/kotlin/BTree.kt
+++ src/main/kotlin/BTree.kt
@@ -1,2 +1,12 @@
-class BTree {
+class BTree() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+
 }
--- src/main/kotlin/BTreeTest.kt
+++ src/main/kotlin/BTreeTest.kt
@@ -1,7 +1,12 @@
-import org.junit.jupiter.api.Assertions.assertAll
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.function.Executable
 
 class BTreeTest {
+
+    @Test
+    fun `EmptyObject`() {
+        val tree = BTree()
+        assertTrue(tree == BTree())
+    }
 }
